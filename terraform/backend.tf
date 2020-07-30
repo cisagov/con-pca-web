00fe945c09743ee501f3e4a8a9dfa10d5d54b558
Zachary Wilson <wilsonzachary634@gmail.com>
2020-07-30 10:52:31 -0600

    deploy develop

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,5 @@
 terraform {
   backend "s3" {
-    bucket         = "con-pca-stage-tf-backend"
-    key            = "con-pca-client.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "con-pca-tf-lock"
+
   }
 }
