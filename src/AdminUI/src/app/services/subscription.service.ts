d4ad6a4c32e8ab5d028982ff8d379f21648eacd5
Zachary Wilson <wilsonzachary634@gmail.com>
2020-08-11 11:17:31 -0600

    use new api endpoint for sending report

--- src/AdminUI/src/app/services/subscription.service.ts
+++ src/AdminUI/src/app/services/subscription.service.ts
@@ -241,18 +241,18 @@ export class SubscriptionService {
     return this.http.get(url, { headers: headers, responseType: 'blob' });
   }
 
-  public sendMonthlyReport(uuid: string) {
-    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/monthly/`;
+  public sendMonthlyReport(uuid: string, date) {
+    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/monthly/${date}/`;
     return this.http.get(url);
   }
 
-  public sendCycleReport(uuid: string) {
-    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/cycle/`;
+  public sendCycleReport(uuid: string, date) {
+    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/cycle/${date}/`;
     return this.http.get(url);
   }
 
-  public sendYearlyReport(uuid: string) {
-    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/yearly/`;
+  public sendYearlyReport(uuid: string, date) {
+    const url = `${this.settingsService.settings.apiUrl}/api/v1/reports/${uuid}/email/yearly/${date}/`;
     return this.http.get(url);
   }
 
