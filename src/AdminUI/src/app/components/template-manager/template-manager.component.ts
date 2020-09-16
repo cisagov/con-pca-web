6e8d241bbfd2e0c7b505f5f555b3dd0d1dd1f458
Zachary Wilson <wilsonzachary634@gmail.com>
2020-09-16 11:56:52 -0600

    set sender/display name independently

--- src/AdminUI/src/app/components/template-manager/template-manager.component.ts
+++ src/AdminUI/src/app/components/template-manager/template-manager.component.ts
@@ -1,15 +1,29 @@
-
-
-import { Component, OnInit, ViewChild, ElementRef, ChangeDetectorRef, AfterViewInit } from '@angular/core';
-import { FormControl, Validators, FormGroup, RangeValueAccessor } from '@angular/forms';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  ChangeDetectorRef,
+  AfterViewInit,
+} from '@angular/core';
+import {
+  FormControl,
+  Validators,
+  FormGroup,
+  RangeValueAccessor,
+} from '@angular/forms';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
 import { Router, ActivatedRoute } from '@angular/router';
 import { MyErrorStateMatcher } from 'src/app/helper/ErrorStateMatcher';
 import { LayoutMainService } from 'src/app/services/layout-main.service';
 import { TemplateManagerService } from 'src/app/services/template-manager.service';
-import { Template, TagModel, GoPhishTemplate } from 'src/app/models/template.model';
+import {
+  Template,
+  TagModel,
+  GoPhishTemplate,
+} from 'src/app/models/template.model';
 import { Subscription as PcaSubscription } from 'src/app/models/subscription.model';
-import { Subscription } from 'rxjs';
+import { from, Subscription } from 'rxjs';
 import $ from 'jquery';
 import 'src/app/helper/csvToArray';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
@@ -25,12 +39,15 @@ import { AlertComponent } from '../dialogs/alert/alert.component';
 import { LandingPageManagerService } from 'src/app/services/landing-page-manager.service';
 import { Landing_Page } from 'src/app/models/landing-page.models';
 import { SendingProfile } from 'src/app/models/sending-profile.model';
-import Swal from 'sweetalert2'
-
+import Swal from 'sweetalert2';
 
 import { SendingProfileService } from 'src/app/services/sending-profile.service';
 import { TestEmail } from 'src/app/models/test-email.model';
-import { DomSanitizer, SafeResourceUrl, SafeUrl} from '@angular/platform-browser';
+import {
+  DomSanitizer,
+  SafeResourceUrl,
+  SafeUrl,
+} from '@angular/platform-browser';
 import { Variable } from '@angular/compiler/src/render3/r3_ast';
 import { filterSendingProfiles } from '../../helper/utilities';
 
@@ -40,20 +57,18 @@ import { filterSendingProfiles } from '../../helper/utilities';
   templateUrl: './template-manager.component.html',
 })
 export class TemplateManagerComponent implements OnInit, AfterViewInit {
-
   dialogRefConfirm: MatDialogRef<ConfirmComponent>;
   dialogRefTagSelection: MatDialogRef<TagSelectionComponent>;
   dialogRefRetire: MatDialogRef<RetireTemplateDialogComponent>;
 
-  //Full template list variables
-  search_input: string;
+  // Full template list variables
   sendingProfiles = [];
-  testEmail = "";
-  firstName = "";
-  lastName = "";
-  role = "";
+  testEmail = '';
+  firstName = '';
+  lastName = '';
+  role = '';
 
-  //Body Form Variables
+  // Body Form Variables
   templateId: string;
   retired: boolean;
   retiredReason: string;
@@ -62,9 +77,11 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
   matchFromAddress = new MyErrorStateMatcher();
   matchTemplateName = new MyErrorStateMatcher();
   matchTemplateHTML = new MyErrorStateMatcher();
+  fromDisplayName: string;
+  fromSender: string;
 
-  mailtester_iframe_url = this.cleanURL("");
-  currentTab = "HTML View"
+  mailtester_iframe_url = this.cleanURL('');
+  currentTab = 'HTML View';
 
   //RxJS Subscriptions
   subscriptions = Array<Subscription>();
@@ -87,14 +104,13 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
   text_editor_height2: number;
   iframe_height: number;
   text_area_bot_marg: number = 20; //based on the default text area padding on a mat textarea element
-  angular_editor_mode: String = "WYSIWYG"
+  angular_editor_mode: String = 'WYSIWYG';
 
   //Elements used to get reference sizes for styling
   @ViewChild('selectedTemplateTitle') titleElement: ElementRef;
   @ViewChild('tabs') tabElement: any;
   @ViewChild('angularEditor') angularEditorEle: any;
   submitted: boolean = true;
-
 
   constructor(
     private layoutSvc: LayoutMainService,
@@ -106,9 +122,9 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     private router: Router,
     private settingsService: SettingsService,
     public dialog: MatDialog,
-    private domSanitizer : DomSanitizer,
+    private domSanitizer: DomSanitizer,
     private cdr: ChangeDetectorRef,
-    private elRef: ElementRef,
+    private elRef: ElementRef
   ) {
     //layoutSvc.setTitle('Edit Template');
     //this.setEmptyTemplateForm();
@@ -124,7 +140,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
         //Use preset empty form
         layoutSvc.setTitle('New Template');
       }
-    })
+    });
   }
 
   /**
@@ -164,21 +180,21 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
       })
     );
 
-    this.landingPageSvc.getAlllandingpages(false,true).subscribe((data: any) =>{
-      this.pagesList = data;
-    });
+    this.landingPageSvc
+      .getAlllandingpages(false, true)
+      .subscribe((data: any) => {
+        this.pagesList = data;
+      });
     this.sendingProfileSvc.getAllProfiles().subscribe((data: any) => {
       this.sendingProfiles = filterSendingProfiles(data);
     });
-
   }
 
-
-  toggleEditorMode(event){
-    if($("#justifyLeft-").is(":disabled")){
-      this.angular_editor_mode = "WYSIWYG"
+  toggleEditorMode(event) {
+    if ($('#justifyLeft-').is(':disabled')) {
+      this.angular_editor_mode = 'WYSIWYG';
     } else {
-      this.angular_editor_mode = "Text"
+      this.angular_editor_mode = 'Text';
     }
   }
 
@@ -192,15 +208,15 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
   ngAfterViewInit() {
     this.configAngularEditor();
     this.addInsertTagButtonIntoEditor();
-    $("#toggleEditorMode-").on('mousedown', this.toggleEditorMode.bind(this))
-    $(".mat-tab-label").on('mousedown', this.matTabChange.bind(this))
+    $('#toggleEditorMode-').on('mousedown', this.toggleEditorMode.bind(this));
+    $('.mat-tab-label').on('mousedown', this.matTabChange.bind(this));
   }
 
-  matTabChange(event){
-    if(this.currentTab == "HTML View"){
-      if(this.angular_editor_mode == "Text"){
-        $("#toggleEditorMode-").click()
-        this.angular_editor_mode = "WYSIWYG"
+  matTabChange(event) {
+    if (this.currentTab == 'HTML View') {
+      if (this.angular_editor_mode == 'Text') {
+        $('#toggleEditorMode-').click();
+        this.angular_editor_mode = 'WYSIWYG';
       }
     }
   }
@@ -222,6 +238,13 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
         },
         { emitEvent: false }
       );
+
+      this.fromSender = this.currentTemplateFormGroup.controls[
+        'templateFromSender'
+      ].value;
+      this.fromDisplayName = this.currentTemplateFormGroup.controls[
+        'templateFromDisplayName'
+      ].value;
     });
   }
 
@@ -262,14 +285,20 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
       template.behavior = <any>{};
     }
 
+    console.log(template);
+    console.log(template.from_address);
+    this.setTemplateFrom(template.from_address);
+
     this.currentTemplateFormGroup = new FormGroup({
       templateUUID: new FormControl(template.template_uuid),
       templateName: new FormControl(template.name, [Validators.required]),
       templateDeceptionScore: new FormControl(template.deception_score),
       templateDescriptiveWords: new FormControl(template.descriptive_words),
       templateDescription: new FormControl(template.description),
+      templateFromDisplayName: new FormControl(this.fromDisplayName),
+      templateFromSender: new FormControl(this.fromSender),
       templateFromAddress: new FormControl(template.from_address),
-      sendingProfile: new FormControl(""),
+      sendingProfile: new FormControl(''),
       templateSubject: new FormControl(template.subject, [Validators.required]),
       templateText: new FormControl(template.text),
       templateHTML: new FormControl(template.html, [Validators.required]),
@@ -313,7 +342,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     );
   }
 
-  replaceEscapeSequence(value: string){
+  replaceEscapeSequence(value: string) {
     let rval = value.split('&lt;%').join('<%');
     rval = rval.split('%&gt;').join('%>');
     return rval;
@@ -330,7 +359,9 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     );
 
     let formTemplate = new Template(form.value);
-    let htmlValue = this.replaceEscapeSequence(form.controls['templateHTML'].value);
+    let htmlValue = this.replaceEscapeSequence(
+      form.controls['templateHTML'].value
+    );
     let saveTemplate = new Template({
       template_uuid: form.controls['templateUUID'].value,
       name: form.controls['templateName'].value,
@@ -338,7 +369,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
       deception_score: form.controls['templateDeceptionScore'].value,
       descriptive_words: form.controls['templateDescriptiveWords'].value,
       description: form.controls['templateDescription'].value,
-      from_address: form.controls['templateFromAddress'].value,
+      from_address: this.getTemplateFrom(),
       subject: form.controls['templateSubject'].value,
       text: form.controls['templateText'].value,
       html: htmlValue,
@@ -375,7 +406,6 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     this.router.navigate(['/templates']);
   }
 
-
   /**
    *
    */
@@ -384,9 +414,9 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     this.currentTemplateFormGroup.markAllAsTouched();
     if (this.currentTemplateFormGroup.valid) {
       //check editor mode, change to wysiwg mode if in html text mode
-      if(this.angular_editor_mode == "Text"){
-        $("#toggleEditorMode-").trigger("click")
-        this.angular_editor_mode = "WYSIWYG"
+      if (this.angular_editor_mode == 'Text') {
+        $('#toggleEditorMode-').trigger('click');
+        this.angular_editor_mode = 'WYSIWYG';
       }
       let templateToSave = this.getTemplateFromForm(
         this.currentTemplateFormGroup
@@ -438,7 +468,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
       const controls = this.currentTemplateFormGroup.controls;
       for (const name in controls) {
         if (controls[name].invalid) {
-          let nameIng = 'Tempalte ' + name.replace(/template/g,'');
+          let nameIng = 'Tempalte ' + name.replace(/template/g, '');
           invalid.push(nameIng);
         }
       }
@@ -522,7 +552,9 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     this.subscriptionSvc
       .getSubscriptionsByTemplate(template_to_stop)
       .subscribe((data: any[]) => {
-        data =  data.filter((subscription) => subscription.status === "In Progress");
+        data = data.filter(
+          (subscription) => subscription.status === 'In Progress'
+        );
         if (data.length > 0) {
           this.dialog.open(StopTemplateDialogComponent, {
             data: template_to_stop,
@@ -531,7 +563,8 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
           this.dialog.open(AlertComponent, {
             data: {
               title: 'Stop Template',
-              messageText: 'There are no Subscriptions currently In Progress with this Template.',
+              messageText:
+                'There are no Subscriptions currently In Progress with this Template.',
             },
           });
         }
@@ -554,9 +587,12 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     let selected_template_height = this.titleElement.nativeElement.offsetHeight;
     //height of the tabs
     let tmpelement = this.tabElement._tabHeader._elementRef.nativeElement;
-    let tab_height = tmpelement && tmpelement.clientHeight || 300;
+    let tab_height = (tmpelement && tmpelement.clientHeight) || 300;
     //height of the space created between teh text area and the bottom of the tab structure
-    let mat_text_area_height = $('.mat-form-field-infix')[0] && $('.mat-form-field-infix')[0].clientHeight || 300;
+    let mat_text_area_height =
+      ($('.mat-form-field-infix')[0] &&
+        $('.mat-form-field-infix')[0].clientHeight) ||
+      300;
     //
     let save_button_row_height = 54;
     //Calculate the height allocated for the text areas, the text-area will use this directly while the editor will require assingment
@@ -567,11 +603,13 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
       mat_text_area_height -
       save_button_row_height;
     this.text_editor_height2 = this.text_editor_height;
-    this.iframe_height = this.text_editor_height-220;
-
+    this.iframe_height = this.text_editor_height - 220;
 
     //Get the angular-editor toolbar height as it changes when the buttons wrap
-    let angular_editor_tool_bar_height = $('.angular-editor-toolbar')[0] && $('.angular-editor-toolbar')[0].clientHeight || 300;
+    let angular_editor_tool_bar_height =
+      ($('.angular-editor-toolbar')[0] &&
+        $('.angular-editor-toolbar')[0].clientHeight) ||
+      300;
     //Set the editorConfig height to the text area height minus the toolbar height
     this.editorConfig['height'] =
       this.text_editor_height - angular_editor_tool_bar_height + 'px';
@@ -687,7 +725,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     selection.insertNode(newNode);
   }
 
-  openLandingPageEditor(){
+  openLandingPageEditor() {
     this.router.navigate(['/landing-pages']);
   }
 
@@ -699,7 +737,7 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     form.controls['templateText'].setValue(
       this.angularEditorEle.textArea.nativeElement.innerText
     );
-    let saveTemplate: GoPhishTemplate ={
+    let saveTemplate: GoPhishTemplate = {
       attachments: [],
       name: form.controls['templateName'].value,
       subject: form.controls['templateSubject'].value,
@@ -710,60 +748,101 @@ export class TemplateManagerComponent implements OnInit, AfterViewInit {
     return saveTemplate;
   }
 
-  onSendTestClick(){
+  onSendTestClick() {
     this.submitted = true;
 
-    const sp :SendingProfile = this.f.sendingProfile.value;
-    if(!sp){
-      Swal.fire("sending profile to test email is required");
+    const sp: SendingProfile = this.f.sendingProfile.value;
+    if (!sp) {
+      Swal.fire('sending profile to test email is required');
       return;
     }
-    if(sp==null){
-      Swal.fire("sending profile to test email is required");
+    if (sp == null) {
+      Swal.fire('sending profile to test email is required');
       return;
     }
-    if(sp==undefined){
-      Swal.fire("sending profile to test email is required");
+    if (sp == undefined) {
+      Swal.fire('sending profile to test email is required');
       return;
     }
 
     //need to go get the sending profile from gophish
-    let tmp_template = this.getGophishTemplateFromForm(this.currentTemplateFormGroup);
-    let email_for_test: TestEmail ={
-      template: tmp_template,//template name to be used in the test
+    let tmp_template = this.getGophishTemplateFromForm(
+      this.currentTemplateFormGroup
+    );
+    let email_for_test: TestEmail = {
+      template: tmp_template, //template name to be used in the test
       first_name: this.firstName,
       last_name: this.lastName,
       email: this.testEmail,
       position: this.role,
-      url: "",
-      smtp: sp
+      url: '',
+      smtp: sp,
     };
 
-    this.sendingProfileSvc.sendTestEmail(email_for_test).subscribe((data: any) => {
-      console.log(data);
-      Swal.fire(data.message);
-      const iframesource =  email_for_test.email.split("@");
-      if(iframesource.length>1)
-        this.mailtester_iframe_url = this.cleanURL("https://www.mail-tester.com/"+iframesource[0]);
-      else
-        this.mailtester_iframe_url = this.cleanURL("https://www.mail-tester.com/barryhansen-"+this.getCleanJobName());
-
+    this.sendingProfileSvc.sendTestEmail(email_for_test).subscribe(
+      (data: any) => {
+        console.log(data);
+        Swal.fire(data.message);
+        const iframesource = email_for_test.email.split('@');
+        if (iframesource.length > 1)
+          this.mailtester_iframe_url = this.cleanURL(
+            'https://www.mail-tester.com/' + iframesource[0]
+          );
+        else
+          this.mailtester_iframe_url = this.cleanURL(
+            'https://www.mail-tester.com/barryhansen-' + this.getCleanJobName()
+          );
       },
-    error => {
-        console.log('Error sending test email: ' + (<Error>error).name + (<Error>error).message);
-    });
+      (error) => {
+        console.log(
+          'Error sending test email: ' +
+            (<Error>error).name +
+            (<Error>error).message
+        );
+      }
+    );
   }
-  cleanURL(oldURL){
+  cleanURL(oldURL) {
     return this.domSanitizer.bypassSecurityTrustResourceUrl(oldURL);
   }
 
-  getCleanJobName(){
+  getCleanJobName() {
     let name = this.currentTemplateFormGroup.controls['templateName'].value;
-    const tname = name.replace(/[^a-zA-Z0-9_]/g,'');
+    const tname = name.replace(/[^a-zA-Z0-9_]/g, '');
     return tname;
   }
 
-  getRecommendedEmail(){
-    return "barryhansen-"+this.getCleanJobName()+"@srv1.mail-tester.com";
+  getRecommendedEmail() {
+    return 'barryhansen-' + this.getCleanJobName() + '@srv1.mail-tester.com';
+  }
+
+  setTemplateFrom(fromAddress: string) {
+    if (fromAddress) {
+      // Generate Display Name
+      const spaceSplitArray = fromAddress.split(' ');
+      if (spaceSplitArray.length > 1) {
+        this.fromDisplayName = spaceSplitArray
+          .splice(0, spaceSplitArray.length - 1)
+          .join(' ')
+          .trim();
+      } else {
+        this.fromDisplayName = 'Display Name';
+      }
+
+      // Generate Sender
+      const senderArray = fromAddress.split('@')[0].split('<');
+      this.fromSender = senderArray[senderArray.length - 1].trim();
+      if (!this.fromSender) {
+        this.fromSender = 'sender';
+      }
+    } else {
+      // Default values if not from address
+      this.fromSender = 'sender';
+      this.fromDisplayName = 'Display Name';
+    }
+  }
+
+  getTemplateFrom() {
+    return `${this.fromDisplayName} <${this.fromSender}@domain.com>`;
   }
 }
