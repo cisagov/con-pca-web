fa4bca589ebdc97dd7f43e7fad94e10e7480bf16
Zachary Wilson <wilsonzachary634@gmail.com>
2021-01-29 09:13:53 -0700

    fix build errors

--- src/AdminUI/src/app/models/test-email.model.ts
+++ src/AdminUI/src/app/models/test-email.model.ts
@@ -2,11 +2,10 @@
 //note that the template must be saved
 
 import { SendingProfile } from './sending-profile.model';
-import { Template } from './template.model';
 
 //before a test email can be sent
 export class TestEmail {
-    template: Template;
+    template: any; // Accepts either GophishTemplateModel or TemplateModel
     first_name: string;
     last_name: string;
     email: string;
