8c378b04ee7ca1e83daf4f69e8b8b531c4e3b9c5
Zachary Wilson <zachary.wilson@inl.gov>
2021-04-07 10:13:34 -0600

    add versioning to docker image
    Add version label
    Add version log to startup
    Add version tests

--- tests/container_test.py
+++ tests/container_test.py
@@ -2,21 +2,24 @@
 """Tests for example container."""
 
 # Standard Python Libraries
+import os
 import time
 
 READY_MESSAGE = "Starting nginx"
+RELEASE_TAG = os.getenv("RELEASE_TAG")
+VERSION_FILE = "src/version.txt"
 
 
 def test_container_count(dockerc):
-    """Verify the test composition and container."""
+    """Verify correct number of containers are running."""
     # stopped parameter allows non-running containers in results
     assert (
         len(dockerc.containers(stopped=True)) == 1
     ), "Wrong number of containers were started."
 
 
-def test_wait_for_ready(main_container):
-    """Wait for container to be ready."""
+def test_container(main_container):
+    """Test container runs, has correct logs and has correct version."""
     # At the moment, the entrypoint takes a while, current working
     # this to be shorter, so hence the longer timeout.
     TIMEOUT = 360
@@ -33,6 +36,33 @@ def test_wait_for_ready(main_container):
     # After container is ready, give it some time and make sure
     # it's still running.
     time.sleep(10)
-    assert main_container.is_running is True
-    assert main_container.is_restarting is False
-    assert main_container.exit_code == 0
+
+    # Make sure the container is not exiting.
+    assert main_container.is_running is True, "Docker container is not running."
+    assert main_container.is_restarting is False, "Docker container is restarting."
+    assert main_container.exit_code == 0, "Docker container exited."
+
+    # Get project version
+    pkg_vars = {}
+    with open(VERSION_FILE) as f:
+        exec(f.read(), pkg_vars)  # nosec
+    project_version = pkg_vars["__version__"]
+
+    # Get log output
+    log_output = main_container.logs().decode("utf-8")
+
+    # Assert version output is in the logs.
+    assert (
+        f"Running Con-PCA Version {project_version}" in log_output
+    ), f"Container version output to log does not match project version file {VERSION_FILE}"
+
+    # Assert release Version Label
+    if RELEASE_TAG:
+        assert (
+            RELEASE_TAG == f"v{project_version}"
+        ), "RELEASE_TAG does not match the project version"
+
+    # Assert container version label matches
+    assert (
+        main_container.labels["org.opencontainers.image.version"] == project_version
+    ), "Dockerfile version label does not match project version"
