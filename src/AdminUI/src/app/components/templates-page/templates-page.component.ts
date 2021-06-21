b3da73c94a3468fe4f8f4b6f1f8e22b2bf844abc
Dylan Johnson <dylanj1752@gmail.com>
2021-06-21 15:33:08 -0600

    Download templates as json

--- src/AdminUI/src/app/components/templates-page/templates-page.component.ts
+++ src/AdminUI/src/app/components/templates-page/templates-page.component.ts
@@ -5,6 +5,8 @@ import { TemplateManagerService } from 'src/app/services/template-manager.servic
 import { Template } from 'src/app/models/template.model';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatSort } from '@angular/material/sort';
+import { MatDialog } from '@angular/material/dialog';
+import { AlertComponent } from 'src/app/components/dialogs/alert/alert.component';
 
 @Component({
   selector: '',
@@ -30,7 +32,8 @@ export class TemplatesPageComponent implements OnInit, AfterViewInit {
   constructor(
     private templateSvc: TemplateManagerService,
     private router: Router,
-    private layoutSvc: LayoutMainService
+    private layoutSvc: LayoutMainService,
+    public dialog: MatDialog
   ) {
     layoutSvc.setTitle('Templates');
   }
@@ -66,5 +69,32 @@ export class TemplatesPageComponent implements OnInit, AfterViewInit {
       this.displayedColumns.push('retired');
     }
     this.refresh();
+  }
+  downloadObject(filename, blob) {
+    const a = document.createElement('a');
+    const objectUrl = URL.createObjectURL(blob);
+    a.href = objectUrl;
+    a.download = filename;
+    a.click();
+    URL.revokeObjectURL(objectUrl);
+  }
+
+  downloadTemplatesJSON() {
+    if (confirm('Download all templates?')) {
+      this.templateSvc.getTemplatesJSON(this.showRetired).subscribe(
+        (blob) => {
+          console.log(blob);
+          this.downloadObject(`template_data.json`, blob);
+        },
+        (error) => {
+          this.dialog.open(AlertComponent, {
+            data: {
+              title: 'Error',
+              messageText: `An error occured downloading the template data. Check logs for more detail.`,
+            },
+          });
+        }
+      );
+    }
   }
 }
