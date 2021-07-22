5b277856ca95ae84a2ff89fe8b652bf8312e5f55
Zachary Wilson <zachary.wilson@inl.gov>
2021-07-22 10:37:51 -0600

    display url tag properly

--- src/AdminUI/src/app/components/subscriptions/manage-subscription/template-select-dialog/template-select-dialog.component.ts
+++ src/AdminUI/src/app/components/subscriptions/manage-subscription/template-select-dialog/template-select-dialog.component.ts
@@ -92,11 +92,6 @@ export class TemplateSelectDialogComponent {
     }
   }
 
-  test() {
-    console.log(this.data);
-    console.log('test from modal');
-  }
-
   public filterList = (value: string) => {
     this.selectedList.filter = value.trim().toLocaleLowerCase();
     this.avaiableList.filter = value.trim().toLocaleLowerCase();
@@ -104,10 +99,7 @@ export class TemplateSelectDialogComponent {
 
   display(template: Template) {
     this.templateSelected = true;
-    console.log(template);
-    var re = '<%URL%>';
-    this.displayHTML = template.html.replace(re, 'javascript:void(0)');
-
+    this.displayHTML = template.html;
     this.templateName = template.name;
     this.templateSubject = template.subject;
     this.templateFromName = template.from_address;
