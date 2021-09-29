fd4af9df644b8c691a35c311323e1960d8b2d62a
Zachary Wilson <zachary.wilson@inl.gov>
2021-09-29 14:33:24 -0600

    Manage non human orgs

--- src/AdminUI/src/app/components/user-admin/user-admin.component.ts
+++ src/AdminUI/src/app/components/user-admin/user-admin.component.ts
@@ -1,6 +1,7 @@
 import { SettingsService } from 'src/app/services/settings.service';
 import { Component, OnInit, Input } from '@angular/core';
 import { LayoutMainService } from 'src/app/services/layout-main.service';
+import { NonhumanService } from 'src/app/services/nonhuman.service';
 
 @Component({
   selector: '',
@@ -8,12 +9,37 @@ import { LayoutMainService } from 'src/app/services/layout-main.service';
   styleUrls: ['./user-admin.component.scss'],
 })
 export class UserAdminComponent implements OnInit {
+  orgs = [];
+  newOrg: string;
+
   constructor(
     public layoutSvc: LayoutMainService,
-    public settingsSvc: SettingsService
+    public settingsSvc: SettingsService,
+    public nonhumanSvc: NonhumanService
   ) {
     layoutSvc.setTitle('User Admin');
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.getAsnOrgs();
+  }
+
+  getAsnOrgs() {
+    this.nonhumanSvc.getNonHumanOrgs().subscribe((data: string[]) => {
+      this.orgs = data;
+    });
+  }
+
+  deleteOrg(org: string) {
+    this.nonhumanSvc.deleteNonHumanOrg(org).subscribe(() => {
+      this.getAsnOrgs();
+    });
+  }
+
+  addOrg() {
+    this.nonhumanSvc.saveNonHumanOrg(this.newOrg).subscribe(() => {
+      this.getAsnOrgs();
+      this.newOrg = '';
+    });
+  }
 }
---
+++ src/AdminUI/src/app/services/nonhuman.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { SettingsService } from './settings.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class NonhumanService {
+  url: string;
+
+  constructor(
+    private http: HttpClient,
+    private settingsService: SettingsService
+  ) {
+    this.url = `${this.settingsService.settings.apiUrl}/api/nonhumans/`;
+  }
+
+  public getNonHumanOrgs() {
+    return this.http.get(this.url);
+  }
+
+  public saveNonHumanOrg(org) {
+    return this.http.post(this.url, org);
+  }
+
+  public deleteNonHumanOrg(org) {
+    return this.http.request('DELETE', this.url, { body: org });
+  }
+}
