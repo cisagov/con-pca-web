3e69e4571ec2cdfb4b9cc7c4893bbac99d9cfc86
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-03-28 11:17:51 -0700

    update aggregate stats tab to overview

--- src/AdminUI/src/app/components/overview/aggregate-statistics-tab/aggregate-statistics-tab.component.ts
+++ src/AdminUI/src/app/components/overview/aggregate-statistics-tab/aggregate-statistics-tab.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AggregateReportModel } from 'src/app/models/reports.model';
+import { StatisticsService } from 'src/app/services/statistics.service';
 
 @Component({
   selector: 'app-aggregate-statistics-tab',
   templateUrl: './aggregate-statistics-tab.component.html',
 })
-export class AggregateStatisticsTab {}
+export class AggregateStatisticsTab implements OnInit {
+  detail: AggregateReportModel;
+
+  constructor(public statsSvc: StatisticsService) {}
+
+  ngOnInit(): void {
+    this.statsSvc.getAggregateStats().subscribe(
+      (result: AggregateReportModel) => {
+        this.detail = result;
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+}
