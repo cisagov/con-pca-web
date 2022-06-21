68ffd3e752ec35d9073b9014c2e5d9e700861220
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-06-21 15:11:43 -0700

    add delete cycle component

---
+++ src/AdminUI/src/app/components/subscriptions/delete-cycle/delete-cycle.component.ts
@@ -0,0 +1,84 @@
+import { Component, Inject, Input } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA,
+} from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { SubscriptionModel } from 'src/app/models/subscription.model';
+import { SubscriptionService } from 'src/app/services/subscription.service';
+import { Router } from '@angular/router';
+
+export interface DialogData {
+  confirmName: string;
+  subscription: SubscriptionModel;
+}
+
+@Component({
+  selector: 'delete-cycle',
+  templateUrl: 'delete-cycle.component.html',
+  styleUrls: ['delete-cycle.component.scss'],
+})
+export class DeleteCycle {
+  @Input()
+  subscription: SubscriptionModel;
+
+  constructor(
+    public dialog: MatDialog,
+    private _snackBar: MatSnackBar,
+    public cycleSvc: SubscriptionService,
+    private router: Router
+  ) {}
+
+  openDialog(): void {
+    const dialogRef = this.dialog.open(DeleteCycleDialog, {
+      width: '450px',
+      data: { subscription: this.subscription },
+    });
+
+    dialogRef.afterClosed().subscribe((result) => {
+      if (result) {
+        this.cycleSvc.deleteSubscription(this.subscription).subscribe(
+          (success) => {
+            this.router.navigate(['/subscriptions']);
+          },
+          (error) => {
+            let snackBarRef = this._snackBar.open(
+              'Cycle Deletion Failed',
+              'Dismiss',
+              { duration: 5000 }
+            );
+          }
+        );
+      }
+    });
+  }
+}
+
+@Component({
+  selector: 'delete-cycle-dialog',
+  templateUrl: 'delete-cycle-dialog.html',
+})
+export class DeleteCycleDialog {
+  subscription: SubscriptionModel;
+  canDelete: boolean;
+
+  constructor(
+    public dialogRef: MatDialogRef<DeleteCycleDialog>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+    this.canDelete = false;
+    this.subscription = data.subscription;
+  }
+
+  confirmValueChange(value) {
+    if (value === this.subscription.name) {
+      this.canDelete = true;
+    } else {
+      this.canDelete = false;
+    }
+  }
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
