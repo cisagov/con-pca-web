efdfd5591aeacf1c3eb8cc46b136ce612c24b41d
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-06-23 16:23:52 -0700

    add logging table to logging tab

--- src/AdminUI/src/app/components/overview/logging-tab/logging-tab.component.ts
+++ src/AdminUI/src/app/components/overview/logging-tab/logging-tab.component.ts
@@ -1,9 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { LoggingModel } from 'src/app/models/logging.model';
+import { LoggingService } from 'src/app/services/logging.service';
 
 @Component({
   selector: 'app-logging-tab',
   templateUrl: './logging-tab.component.html',
 })
 export class LoggingTab implements OnInit {
-  ngOnInit(): void {}
+  @ViewChild('loggingTable', { read: MatSort, static: true })
+  sortLogging: MatSort;
+  loading = false;
+
+  // Logging Messages Table
+  public loggingSource: MatTableDataSource<LoggingModel>;
+  loggingDisplayedColumns = ['timestamp', 'message'];
+
+  constructor(private loggingSvc: LoggingService) {}
+
+  ngOnInit(): void {
+    this.loggingSource = new MatTableDataSource();
+    this.refresh();
+  }
+
+  async refresh() {
+    this.loading = true;
+    await this.loggingSvc.getLogging().subscribe((logging: LoggingModel[]) => {
+      this.loggingSource.sort = this.sortLogging;
+      this.loggingSource.data = logging;
+      this.loading = false;
+    });
+  }
 }
