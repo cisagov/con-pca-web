1db2ffa890e59ed4b569101e02d78478bef6ca55
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-06-23 15:51:32 -0700

    add logging tab and service

---
+++ src/AdminUI/src/app/components/overview/logging-tab/logging-tab.component.ts
@@ -0,0 +1,9 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-logging-tab',
+  templateUrl: './logging-tab.component.html',
+})
+export class LoggingTab implements OnInit {
+  ngOnInit(): void {}
+}
