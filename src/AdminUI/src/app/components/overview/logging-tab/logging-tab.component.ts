8e11e811d1388f9c47ed04b38a00b78b1168479a
Mostafa Abdelbaky <mostafa.abdelbaky@inl.gov>
2022-06-23 17:11:16 -0700

    display logging content

--- src/AdminUI/src/app/components/overview/logging-tab/logging-tab.component.ts
+++ src/AdminUI/src/app/components/overview/logging-tab/logging-tab.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 import { LoggingModel } from 'src/app/models/logging.model';
 import { LoggingService } from 'src/app/services/logging.service';
 
 @Component({
   selector: 'app-logging-tab',
   templateUrl: './logging-tab.component.html',
+  styleUrls: ['./logging-tab.component.scss'],
 })
 export class LoggingTab implements OnInit {
   @ViewChild('loggingTable', { read: MatSort, static: true })
@@ -17,7 +19,7 @@ export class LoggingTab implements OnInit {
   public loggingSource: MatTableDataSource<LoggingModel>;
   loggingDisplayedColumns = ['timestamp', 'message'];
 
-  constructor(private loggingSvc: LoggingService) {}
+  constructor(private loggingSvc: LoggingService, private router: Router) {}
 
   ngOnInit(): void {
     this.loggingSource = new MatTableDataSource();
@@ -28,8 +30,12 @@ export class LoggingTab implements OnInit {
     this.loading = true;
     await this.loggingSvc.getLogging().subscribe((logging: LoggingModel[]) => {
       this.loggingSource.sort = this.sortLogging;
-      this.loggingSource.data = logging;
+      this.loggingSource.data = logging as LoggingModel[];
       this.loading = false;
     });
+  }
+
+  pageRefresh(): void {
+    this.refresh();
   }
 }
